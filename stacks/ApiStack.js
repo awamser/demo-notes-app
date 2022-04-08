d2b231b5391a878096612180768e6c74fa3f2413
Alan Wamser <alan@wamser.com>
2022-04-08 18:00:16 -0500

    Enabling CORS

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -12,6 +12,7 @@ export default class ApiStack extends sst.Stack {
     // Create the API
     this.api = new sst.Api(this, 'Api', {
       defaultAuthorizationType: 'AWS_IAM',
+      cors: true,
       defaultFunctionProps: {
         environment: {
           TABLE_NAME: table.tableName,
