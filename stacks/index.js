d22ed78d03eb3027f50ae274dde5e4743d1b8df6
Alan Wamser <alan@wamser.com>
2022-04-18 18:18:50 -0500

    wtf

--- stacks/index.js
+++ stacks/index.js
@@ -5,20 +5,20 @@ import AuthStack from './AuthStack';
 import FrontendStack from './FrontendStack';
 
 export default function main(app) {
-  const storageStack = new StorageStack(app, 'storage');
+    const storageStack = new StorageStack(app, 'storage');
 
-  const apiStack = new ApiStack(app, 'api', {
-    table: storageStack.table
-  });
+    const apiStack = new ApiStack(app, 'api', {
+        table: storageStack.table
+    });
 
-  const authStack = new AuthStack(app, 'auth', {
-    api: apiStack.api,
-    bucket: storageStack.bucket
-  });
+    const authStack = new AuthStack(app, 'auth', {
+        api: apiStack.api,
+        bucket: storageStack.bucket
+    });
 
-  new FrontendStack(app, 'frontend', {
-    api: apiStack.api,
-    auth: authStack.auth,
-    bucket: storageStack.bucket
-  });
+    new FrontendStack(app, 'frontend', {
+        api: apiStack.api,
+        auth: authStack.auth,
+        bucket: storageStack.bucket
+    });
 }
