ffafde3bffd6c450ab267703bc2e49ae3544f3e2
Alan Wamser <alan@wamser.com>
2022-04-19 21:57:40 -0500

    signup

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
+import Signup from './containers/Signup';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
-        <Home />
+        <Home/>
       </Route>
       <Route exact path="/login">
-        <Login />
+        <Login/>
+      </Route>
+      <Route exact path="/signup">
+        <Signup/>
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
-        <NotFound />
+        <NotFound/>
       </Route>
     </Switch>
   );
