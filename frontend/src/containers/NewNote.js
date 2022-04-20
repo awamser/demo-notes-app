6fc24bccb14713df23fa5917860d68e4ec574cea
Alan Wamser <alan@wamser.com>
2022-04-19 23:53:15 -0500

    settings

--- frontend/src/containers/NewNote.js
+++ frontend/src/containers/NewNote.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import {useHistory} from 'react-router-dom';
 import LoaderButton from '../components/LoaderButton';
 import {API} from 'aws-amplify';
+import {s3Upload} from '../lib/awsLib';
 import {onError} from '../lib/errorLib';
 import config from '../config';
 import './NewNote.css';
@@ -32,13 +33,16 @@ export default function NewNote() {
     setIsLoading(true);
 
     try {
-      await createNote({content});
+      const attachment = file.current ? await s3Upload(file.current) : null;
+      await createNote({content, attachment});
       history.push('/');
+
     } catch (e) {
       onError(e);
       setIsLoading(false);
     }
   }
+
 
   function createNote(note) {
     return API.post('notes', '/notes', {
