07a8254c98919a1e492795069854532295b4bb0a
Alan Wamser <alan@wamser.com>
2022-04-24 21:26:18 -0500

    auth routes

--- frontend/src/containers/Settings.js
+++ frontend/src/containers/Settings.js
@@ -6,16 +6,7 @@ import {onError} from '../lib/errorLib';
 import config from '../config';
 
 export default function Settings() {
-  // const history = useHistory();
-  // const [isLoading, setIsLoading] = useState(false);
-  // const stripePromise = loadStripe(config.STRIPE_KEY);
-  //
-  // function billUser(details) {
-  //   return API.post('notes', '/billing', {
-  //     body: details
-  //   });
-  // }
-
+  
   return (
     <div className="Settings">
       <h2> Setting Go Here!</h2>
